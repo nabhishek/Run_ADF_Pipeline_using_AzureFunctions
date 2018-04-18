ced5506acf4e00dbf9a587baad176907fec1b177
Abhishek Narain <narain.abhishek@gmail.com>
2018-04-18 11:18:59 +0800

    Add project files.

---
+++ RunPipeline-AzureFunctions/Function1.cs
@@ -0,0 +1,42 @@
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Azure.Management.DataFactory;
+using Microsoft.Azure.Management.DataFactory.Models;
+using Microsoft.IdentityModel.Clients.ActiveDirectory;
+using Microsoft.Rest;
+
+namespace RunPipelineAzureFunctions
+{
+    public static class Function1
+    {
+        [FunctionName("Function1")]
+        public static async Task<HttpResponseMessage> Run([HttpTrigger(AuthorizationLevel.Function, "get", Route = null)]HttpRequestMessage req, TraceWriter log)
+        {
+            // Set variables
+            string tenantID = "xxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxx"; //"<your tenant ID>";
+            string applicationId = "xxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxxx"; //"<your application ID>";
+            string authenticationKey = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";//"<your authentication key for the application>";
+            string subscriptionId = "xxxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxxx"; //"<your subscription ID where the data factory resides>";
+            string resourceGroup = "xxxx"; //"<your resource group where the data factory resides>";
+            string dataFactoryName = "xxxx"; //" < specify the name of data factory to create. It must be globally unique.>";
+            string pipelineName = "xxxx";    // name of the pipeline 
+
+            // Authenticate and create a data factory management client
+            var context = new AuthenticationContext("https://login.windows.net/" + tenantID);
+            ClientCredential cc = new ClientCredential(applicationId, authenticationKey);
+            AuthenticationResult result = context.AcquireTokenAsync("https://management.azure.com/", cc).Result;
+            ServiceClientCredentials cred = new TokenCredentials(result.AccessToken);
+            var client = new DataFactoryManagementClient(cred) { SubscriptionId = subscriptionId };
+
+            CreateRunResponse runResponse = client.Pipelines.CreateRunWithHttpMessagesAsync(resourceGroup, dataFactoryName, pipelineName).Result.Body;
+            //log.Info("Pipline run ID: " + runResponse.RunId);
+
+            return req.CreateResponse(HttpStatusCode.OK, "Pipeline run ID: " + runResponse.RunId);         
+        }
+    }
+}
